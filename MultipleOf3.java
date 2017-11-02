41c84dc81a13c01283ca6a06d7120f5007421658
nimalvalath <nimal.j.valath@gmail.com>
2017-11-02 10:50:34 +0530

    Add files via upload

---
+++ MultipleOf3.java
@@ -0,0 +1,12 @@
+class MultipleOf3{
+	public void nultipledownwards(){
+		for(int i=300;i>=3;i-=3){
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		MultipleOf3 m=new MultipleOf3();
+		m.nultipledownwards();
+	}
+
+}
