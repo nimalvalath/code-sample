0b9c54f29d7886af27995f65d1571ac21e51810d
nimalvalath <nimal.j.valath@gmail.com>
2017-11-03 12:00:32 +0530

    Add files via upload

--- pyramidstar.java
+++ pyramidstar.java
@@ -6,27 +6,62 @@ class PyramidStar{
 		System.out.println("Enter height of triangle: ");
 		int height=sc.nextInt();
 		System.out.println("    *     ");
+		System.out.println("   **    ");
+		System.out.println("  ***   ");
+		System.out.println(" ****  ");
+		System.out.println("*****");
+		printPyramidStar1(height);
+		System.out.println();
+		System.out.println("    *     ");
 		System.out.println("   * *    ");
 		System.out.println("  * * *   ");
 		System.out.println(" * * * *  ");
-		System.out.println("* * * * * ");
-		printStar();
+		System.out.println("* * * * *");
+		printPyramidStar2(height);
+		System.out.println();
 
 	}
-	public static void printStar(){
+/*	public static void printStar(int height){
 		System.out.println("print");
-		for(int i=0;i<5;i++){
-			
-			for(int j=1;j<=10;j++){
-				/*System.out.println(i);
-				System.out.println(j);*/
-				if(i==0 &&  j==5){
+			int i=0;
+			while(i<height){
+				for(int j=1;j<=(2*height);j++){
+				System.out.println(i);
+				System.out.println(j);
+				if(j==(height-i)){
 					System.out.print("*");
 
 				}
 				else
 					System.out.print(" ");
 			}
+			System.out.println("\n");
+			i++;
+			}
+			*/
+	public static void printPyramidStar1(int height){
+		for(int i=1;i<=height;i++){
+			for(int j=1;j<=height-i;j++){
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++){
+				System.out.print("*");
+			}
+			System.out.println();
 		}
+		
 	}
+
+	public static void printPyramidStar2(int height){
+		for(int i=1;i<=height;i++){
+			for(int j=1;j<=height-i;j++){
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}	
 }
